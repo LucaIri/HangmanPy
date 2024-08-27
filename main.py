863943d52747b41ed50bb8fe0d7c2e4d62790e41
Luca Irimie <lucaniri@umich.edu>
2024-08-27 17:32:20 -0400

    underscores replaced when guess is correct(not fin)

--- main.py
+++ main.py
@@ -11,30 +11,38 @@ def greeting(target):
             underscore_target += '_'
         else:
             underscore_target += ' '
-    list(underscore_target)
-    joined_underscore_target = " ".join(underscore_target)
-    print(joined_underscore_target)
+    
+    underscore_target_output(underscore_target)
     hint_message = "\nHINT: country names ;)\n"
     print(hint_message)
-    return joined_underscore_target
+    return underscore_target
+
+def underscore_target_output(target):
+    list(target)
+    joined_underscore_target = " ".join(target)
+    print(joined_underscore_target)
+    
 
 def target_word_selection(word_list):
     target_word = random.choice(word_list)
     return target_word
 
-def player_guess(target,unsolved):
+def player_guess(target, unsolved):
     guess = input("Guess a letter!: \n").upper()
     if len(guess)>1:
         print("Only 1 character at a time! Try Again")
         player_guess(target)
     if guess in target:
-        
+        for i in range(len(unsolved)):
+            if target[i] == guess:
+                unsolved = unsolved.replace(unsolved[i], guess)
+    print(unsolved)
     
     
 
 guess_wrong_limit = 10
 target_word_list = ["ARGENTINA", "ANGOLA", "BRAZIL", "BOLIVIA","CANADA", "UNITED STATES", "DOMINICAN REPUBLIC", "RUSSIA", "SPAIN", "FRANCE", "MONGOLIA", "CHINA"]
 target_word = target_word_selection(target_word_list)
-unsolved_word = greeting(target_word)
-while guess_wrong_limit > 0:
-    player_guess(target_word, unsolved_word)
+underscore_target_no_space = greeting(target_word)
+#while guess_wrong_limit > 0:
+player_guess(target_word, underscore_target_no_space)
